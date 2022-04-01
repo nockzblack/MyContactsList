4689296ade2bf801a4b85b5f44499c1125b20413
Fernando Benavides <nockzblack@gmail.com>
2022-04-01 11:36:48 -0600

    Adding Model of the app

---
+++ MyContactsList/Contact.swift
@@ -0,0 +1,18 @@
+//
+//  Contact.swift
+//  MyContactsList
+//
+//  Created by Fernando's Mac on 01/04/22.
+//
+
+import Foundation
+
+class Contact {
+    var avatarName: String
+    var personName: String
+    
+    init(avatarName: String, personName: String) {
+        self.avatarName = avatarName
+        self.personName = personName
+    }
+}
